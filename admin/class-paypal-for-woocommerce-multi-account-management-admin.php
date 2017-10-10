c39832e82636a8cef6c53ea8e46c2ff0c2808116
Kcppdevelopers <kcppdevelopers@gmail.com>
2017-10-10 18:48:09 +0530

    add simple tooltip next to the account setup fields, ref #3

--- admin/class-paypal-for-woocommerce-multi-account-management-admin.php
+++ admin/class-paypal-for-woocommerce-multi-account-management-admin.php
@@ -244,6 +244,12 @@ class Paypal_For_Woocommerce_Multi_Account_Management_Admin {
                                 </td>
                             </tr>
                             <tr>
+                                <th></th>
+                                <td>
+                                    <p class="width460"><?php echo __('is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'paypal-for-woocommerce-multi-account-management'); ?> </p>
+                                </td>
+                            </tr>
+                            <tr>
                                 <th scope="row" class="titledesc">
                                     <label for="woocommerce_paypal_express_api_trigger_conditions"><?php echo __('Trigger Conditions', 'paypal-for-woocommerce-multi-account-management'); ?></label>
                                 </th>
