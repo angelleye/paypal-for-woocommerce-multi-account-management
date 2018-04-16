e068346ade45152148764b5926395f2bdd8822d2
kcppdevelopers <kcppdevelopers@gmail.com>
2018-04-16 10:59:27 +0530

    Role based condition triggers, ref #13

--- admin/class-paypal-for-woocommerce-multi-account-management-admin.php
+++ admin/class-paypal-for-woocommerce-multi-account-management-admin.php
@@ -537,7 +537,7 @@ class Paypal_For_Woocommerce_Multi_Account_Management_Admin {
             foreach ($result as $key => $value) {
                 if (!empty($value->ID)) {
                     $microprocessing_array = get_post_meta($value->ID);
-                    if (isset($microprocessing_array['woocommerce_paypal_express_api_user_role'][0]) && in_array($microprocessing_array['woocommerce_paypal_express_api_user_role'][0], $current_user_roles)) {
+                    if (!isset($microprocessing_array['woocommerce_paypal_express_api_user_role'][0]) || in_array($microprocessing_array['woocommerce_paypal_express_api_user_role'][0], $current_user_roles)) {
                         if (!empty($microprocessing_array['woocommerce_paypal_express_api_condition_sign'][0]) && !empty($microprocessing_array['woocommerce_paypal_express_api_condition_value'][0])) {
                             switch ($microprocessing_array['woocommerce_paypal_express_api_condition_sign'][0]) {
                                 case 'equalto':
