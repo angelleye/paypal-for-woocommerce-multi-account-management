7af2944048dc89603e7b245eca3899c341d7ea74
Deepak Maurya <deepakmaurya@hotmail.com>
2020-06-06 20:39:04 +0530

    Multi-account regenerate sequence array caching issue resolved, PFWMA-132

--- includes/class-paypal-for-woocommerce-multi-account-management-payment-load-balancer.php
+++ includes/class-paypal-for-woocommerce-multi-account-management-payment-load-balancer.php
@@ -32,6 +32,10 @@ class Paypal_For_Woocommerce_Multi_Account_Management_Payment_Load_Balancer {
             $express_checkout_accounts = get_option($option_key);
             return $express_checkout_accounts;
         }
+	    $old_express_checkout_accounts = get_option($option_key);
+	    if (empty($old_express_checkout_accounts)) {
+		    $old_express_checkout_accounts = array();
+	    }
 	    $express_checkout_accounts = array();
 
         $args = array(
@@ -54,31 +58,39 @@ class Paypal_For_Woocommerce_Multi_Account_Management_Payment_Load_Balancer {
             'fields' => 'ids'
         );
         $query = new WP_Query($args);
-        if (empty($express_checkout_accounts['default'])) {
-            $express_checkout_accounts['default'] = array('multi_account_id' => 'default', 'is_used' => '', 'is_api_set' => true, 'email' => 'default');
-        }
+
+        if(isset($old_express_checkout_accounts['default']))
+	        $express_checkout_accounts['default'] = $old_express_checkout_accounts['default'];
+        else
+        	$express_checkout_accounts['default'] = array('multi_account_id' => 'default', 'is_used' => '', 'is_api_set' => true, 'email' => 'default');
+
         if (!empty($query->posts) && count($query->posts) > 0) {
             foreach ($query->posts as $key => $post_id) {
-                if (empty($express_checkout_accounts[$post_id])) {
-                    $microprocessing_array = get_post_meta($post_id);
-                    $bool = $this->angelleye_is_multi_account_api_set($microprocessing_array, $environment);
-                    if($bool) {
-                        if($environment) {
-                            $email = isset($microprocessing_array['woocommerce_paypal_express_sandbox_merchant_id'][0]) ? $microprocessing_array['woocommerce_paypal_express_sandbox_merchant_id'][0] : '';
-                        } else {
-                            $email = isset($microprocessing_array['woocommerce_paypal_express_merchant_id'][0]) ? $microprocessing_array['woocommerce_paypal_express_merchant_id'][0] : '';
-                        }
-                        
+	            if(isset($old_express_checkout_accounts[$post_id])){
+		            $express_checkout_accounts[$post_id] = $old_express_checkout_accounts[$post_id];
+	            }else
+		            $express_checkout_accounts[$post_id] = array('multi_account_id' => $post_id, 'is_used' => '', 'is_api_set' => false, 'email' => '');
+
+                $microprocessing_array = get_post_meta($post_id);
+                $bool = $this->angelleye_is_multi_account_api_set($microprocessing_array, $environment);
+                if($bool) {
+                    if($environment) {
+                        $email = isset($microprocessing_array['woocommerce_paypal_express_sandbox_merchant_id'][0]) ? $microprocessing_array['woocommerce_paypal_express_sandbox_merchant_id'][0] : '';
                     } else {
-                        if($environment) {
-                            $email = isset($microprocessing_array['woocommerce_paypal_express_sandbox_email'][0]) ? $microprocessing_array['woocommerce_paypal_express_sandbox_email'][0] : '';
-                        } else {
-                            $email = isset($microprocessing_array['woocommerce_paypal_express_email'][0]) ? $microprocessing_array['woocommerce_paypal_express_email'][0] : '';
-                        }
-                        
+                        $email = isset($microprocessing_array['woocommerce_paypal_express_merchant_id'][0]) ? $microprocessing_array['woocommerce_paypal_express_merchant_id'][0] : '';
                     }
-                    $express_checkout_accounts[$post_id] = array('multi_account_id' => $post_id, 'is_used' => '', 'is_api_set' => $bool, 'email' => $email);
+
+                } else {
+                    if($environment) {
+                        $email = isset($microprocessing_array['woocommerce_paypal_express_sandbox_email'][0]) ? $microprocessing_array['woocommerce_paypal_express_sandbox_email'][0] : '';
+                    } else {
+                        $email = isset($microprocessing_array['woocommerce_paypal_express_email'][0]) ? $microprocessing_array['woocommerce_paypal_express_email'][0] : '';
+                    }
+
                 }
+                $express_checkout_accounts[$post_id]['is_api_set'] = $bool;
+                $express_checkout_accounts[$post_id]['email'] = $email;
+
             }
         }
         update_option($option_key, $express_checkout_accounts);
