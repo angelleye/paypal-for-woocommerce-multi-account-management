7cf7830e33738c79fe2b5285c7b367b8aacc6d30
Deepak Maurya <deepakmaurya@hotmail.com>
2026-03-30 23:39:33 +0530

    Fixed the Settings page layout issues

--- includes/class-paypal-for-woocommerce-multi-account-management.php
+++ includes/class-paypal-for-woocommerce-multi-account-management.php
@@ -309,10 +309,11 @@ class Paypal_For_Woocommerce_Multi_Account_Management {
     }
 
     public function display_plugin_admin_page() {
-        wp_dequeue_style('woocommerce_admin_styles');
-        $this->display_plugin_admin_page_submenu();
-        echo '<style> .button-primary.woocommerce-save-button { display: none; } </style>';
+        echo '<div class="angelleye_pfwma_admin_shell">';
+        echo '<div class="angelleye_pfwma_admin_content">';
+        echo '<style>.woocommerce-save-button { display: none !important; }</style>';
         $this->plugin_admin->display_admin_notice();
+        $this->display_plugin_admin_page_submenu();
         $section = !empty($_GET['section']) ? $_GET['section'] : '';
         switch ($section) {
             case 'add_edit_account':
@@ -327,12 +328,12 @@ class Paypal_For_Woocommerce_Multi_Account_Management {
             default:
                 $this->plugin_admin->angelleye_multi_account_list();
         }
+        echo '</div></div>';
     }
 
     public function display_plugin_admin_page_submenu() {
         ?>
-        </form>
-        <div class="wrap">
+        <div class="wrap angelleye_pfwma_admin_nav">
             <ul class="subsubsub">
                 <li><a href="<?php echo esc_url(admin_url('admin.php?page=wc-settings&tab=multi_account_management')); ?>" class="<?php
                     if (empty($_GET['section'])) {
@@ -356,11 +357,6 @@ class Paypal_For_Woocommerce_Multi_Account_Management {
                     ?>"><?php echo __('Account Payments Log', 'angelleye-paypal-shipment-tracking-woocommerce'); ?></a>  </li>
             </ul>
             <br class="clear">
-            <?php
-            if (!empty($this->message)) {
-                echo '<div id="message" class="updated inline is-dismissible"><p><strong>' . esc_html($this->message) . '</strong></p></div>';
-            }
-            ?>
         </div>
         <?php
     }
